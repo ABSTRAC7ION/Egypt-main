4c4fa87a32e4a1864176384bb54e300d85f04beb
Ibrahim Shady <96144600+ABSTRAC7ION@users.noreply.github.com>
2022-07-16 19:57:31 +0200

    bug fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [
 		preprocess({
 			postcss: true
@@ -11,8 +9,20 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
+
+
 
 export default config;
